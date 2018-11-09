c68cc92c098477803998dd94fff6181789bfb0b5
fernando.lcoutinho <fernando.lcoutinho@CAS527143W1032.senacsp.edu.br>
2018-11-09 20:47:32 -0200

    Veiculo geral, teste

--- projetoEstacionamentoPOO/src/Veiculos/VeiculoGeral.java
+++ projetoEstacionamentoPOO/src/Veiculos/VeiculoGeral.java
@@ -10,5 +10,33 @@ package Veiculos;
  * @author fernando.lcoutinho
  */
 public class VeiculoGeral {
+    private String placa;
+    private String cor;
+    private String modelo;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
     
 }
