6e0a17ab3e7e1394876565466ed0116b933c5ac4
Murilo Amurim <>
2018-11-24 14:44:37 -0200

    Classes DAO sem o pesquisar pq tava errado

--- projetoEstacionamentoPOO/src/DAOs/CarroDAO.java
+++ projetoEstacionamentoPOO/src/DAOs/CarroDAO.java
@@ -5,6 +5,7 @@ package DAOs;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import Cliente.Cliente;
 import Util.ConnectionUtils;
 import Veiculos.Carro;
 import java.sql.Connection;
@@ -22,7 +23,7 @@ public class CarroDAO {
         String sql = "INSERT INTO carro(placa, modelo, chassi, marca, cor,dataentrada,datasaida) VALUES (?,?,?,?,?,?,?)";
         Connection con = null;
         PreparedStatement preparedStatement = null;
-        long millis=System.currentTimeMillis();
+        long millis = System.currentTimeMillis();
         try {
             con = ConnectionUtils.getConnection();
             preparedStatement = con.prepareStatement(sql);
@@ -32,7 +33,7 @@ public class CarroDAO {
             preparedStatement.setString(4, carro.getMarca());
             preparedStatement.setString(6, carro.getCor());
             preparedStatement.setDate(7, new Date(millis));
-            preparedStatement.setDate(8, new Date(0,0,0000));
+            preparedStatement.setDate(8, new Date(0, 0, 0000));
             preparedStatement.execute();
         } finally {
             if (preparedStatement != null && !preparedStatement.isClosed()) {
@@ -43,4 +44,27 @@ public class CarroDAO {
             }
         }
     }
+
+    public void atualizar(Carro carro, Cliente cliente) throws SQLException, Exception {
+        String sql = "UPDATE Veiculo SET modelo=?, chassi=?, cor=?,marca=?,dataentrada=?,datasaida=?,dono=?"
+                + "WHERE (placa=?)";
+        Connection con = null;
+        PreparedStatement prep = null;
+        try {
+            con = ConnectionUtils.getConnection();
+            prep = con.prepareStatement(sql);
+            prep.setString(1, carro.getModelo());
+            prep.setString(2, carro.getChassi());
+            prep.setString(3, carro.getMarca());
+            prep.setString(4, carro.getCor());
+            prep.setString(5, cliente.getNome());
+        } finally {
+            if (prep != null && !prep.isClosed()) {
+                prep.close();
+            }
+            if (con != null && !con.isClosed()) {
+                con.close();
+            }
+        }
+    } 
 }
--- projetoEstacionamentoPOO/src/DAOs/ClienteDAO.java
+++ projetoEstacionamentoPOO/src/DAOs/ClienteDAO.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import Util.ConnectionUtils;
+import Veiculos.Carro;
 import java.sql.Date;
 
 /**
@@ -36,7 +37,60 @@ public class ClienteDAO {
         prep.setString(8, c.getCelular());
         prep.setString(9, c.getCep());
         System.out.println(prep);
-    
+    }
+    public void atualizar(Carro carro, Cliente cliente) throws SQLException, Exception {
+        String sql = "UPDATE Cliente SET endereco=?, nome=?, email=?,telefone=?,datanascimento=?,celular=?,cep=?,carro=?"
+                + "WHERE (cpf=?)";
+        Connection con = null;
+        PreparedStatement prep = null;
+        try {
+            con = ConnectionUtils.getConnection();
+            prep = con.prepareStatement(sql);
+            prep.setString(1, cliente.getNome());
+            prep.setString(2, cliente.getEmail());
+            prep.setString(3, cliente.getTelefone());
+            prep.setDate(4, cliente.getDataNascimento());
+            prep.setString(5, cliente.getCelular());
+            prep.setString(6, cliente.getCep());
+            prep.setString(7, carro.getPlaca());
+        } finally {
+            if (prep != null && !prep.isClosed()) {
+                prep.close();
+            }
+            if (con != null && !con.isClosed()) {
+                con.close();
+            }
+        }
     }
     
+    public void excluir(Cliente cliente) throws SQLException,Exception{
+        //Monta a string de atualização do cliente no BD, utilizando
+        //prepared statement
+        String sql = "DELETE FROM Cliente  WHERE cpf=?";
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+            //Configura os parâmetros do "PreparedStatement"
+            preparedStatement.setString(1, cliente.getCpf());
+
+            //Executa o comando no banco de dados
+            preparedStatement.execute();
+        } finally {
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
 }
