0a640ffb56c039154dddf6de520b754363e5171b
Murilo.Araujo <42453076+MuAmurim@users.noreply.github.com>
2018-11-29 16:15:30 -0200

    Script sql

---
+++ estacionamento.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE estacionamento;
+USE estacionamento;
+
+CREATE TABLE cliente(
+nome varchar(50),
+cpf varchar(16),
+email varchar(50),
+celular varchar(50),
+telefone varchar(50),
+cep varchar(50),
+primary key (cpf));
+
+CREATE TABLE veiculo(
+placa varchar(8),
+modelo varchar(20),
+marca varchar(20),
+porte varchar(20),
+cpfC varchar(16),
+telefone varchar(50),
+horaE time,
+horaS time,
+primary key(placa),
+foreign key(cpfC) references cliente(cpf)
+);
+
+drop database estacionamento;
