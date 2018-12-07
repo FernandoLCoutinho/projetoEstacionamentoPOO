0e4deb677a97f00aa961138fb830d0c5a21960fa
FernandoLCoutinho <37720885+FernandoLCoutinho@users.noreply.github.com>
2018-12-07 20:36:38 -0200

    Requesitos tabela sql e MySQL connector

--- estacionamento.sql
+++ estacionamento.sql
@@ -14,13 +14,16 @@ CREATE TABLE veiculo(
 placa varchar(8),
 modelo varchar(20),
 marca varchar(20),
+cor varchar(20),
 porte varchar(20),
 cpfC varchar(16),
 telefone varchar(50),
 horaE timestamp,
 horaS timestamp,
+tipo varchar(20),
 primary key(placa),
 foreign key(cpfC) references cliente(cpf)
 );
 
+select * from veiculo;
 drop database estacionamento;
